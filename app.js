5d11002aac4c5b35a018b2d4b31f3230a249168b
David Hide <d.hide@live.co.uk>
2018-01-08 17:57:33 +0000

    Corrected config heroku environment variables

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var ejs = require('ejs');
 var Twit = require('twit');
 
 var config = {
-    consumer_key:         'process.env.consumer_key',
-    consumer_secret:      'process.env.consumer_secret',
-    access_token:         'process.env.access_token',
-    access_token_secret:  'process.env.access_token_secret',
+    consumer_key:         process.env.consumer_key,
+    consumer_secret:      process.env.consumer_secret,
+    access_token:         process.env.access_token,
+    access_token_secret:  process.env.access_token_secret
 };
 console.log(config.consumer_key);
 var T = new Twit(config);

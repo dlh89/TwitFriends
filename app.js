d97f38e3f672980a01e19cc43c4673ee293d8b1c
David Hide <d.hide@live.co.uk>
2018-01-08 17:42:30 +0000

    Adjusted config to use environment variables

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var path = require('path');
 var ejs = require('ejs');
 var Twit = require('twit');
 
-var config = require(path.join(__dirname, 'config'));
+var config = {
+    consumer_key:         'process.env.consumer_key',
+    consumer_secret:      'process.env.consumer_secret',
+    access_token:         'process.env.access_token',
+    access_token_secret:  'process.env.access_token_secret',
+};
 var T = new Twit(config);
 
 var port = process.env.PORT || 3000;
